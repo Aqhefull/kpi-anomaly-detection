53eb5a4438434db9245028f7ce16c88c219ebd9a
Alexander <komarevich.alex@gmail.com>
2018-11-19 23:13:57 +0200

    Updates

---
+++ precache-manifest.9c7c89be9a411465a1563422b6752f60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b928cd4b344c00633d17cbddd63d869",
+    "url": "/kpi-anomaly-detection/static/media/loading.3b928cd4.gif"
+  },
+  {
+    "revision": "b07beab4a6fbcd0ff009",
+    "url": "/kpi-anomaly-detection/static/js/runtime~main.b07beab4.js"
+  },
+  {
+    "revision": "ca8a32815e38cfe37c57",
+    "url": "/kpi-anomaly-detection/static/js/main.ca8a3281.chunk.js"
+  },
+  {
+    "revision": "ad51bdb84f75a7b52ab7",
+    "url": "/kpi-anomaly-detection/static/js/1.ad51bdb8.chunk.js"
+  },
+  {
+    "revision": "ca8a32815e38cfe37c57",
+    "url": "/kpi-anomaly-detection/static/css/main.cbe39e5c.chunk.css"
+  },
+  {
+    "revision": "ad51bdb84f75a7b52ab7",
+    "url": "/kpi-anomaly-detection/static/css/1.083d1e90.chunk.css"
+  },
+  {
+    "revision": "ffbf6555d22217823ee2e27a388dc1cc",
+    "url": "/kpi-anomaly-detection/index.html"
+  }
+];
